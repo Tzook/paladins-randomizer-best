ffe21c3efd479a1688de387bd36f2339bd6dd429
Tzook <tzook10@gmail.com>
2021-04-22 09:46:17 +0300

    smol fix

--- react-ui/src/Champs.js
+++ react-ui/src/Champs.js
@@ -88,9 +88,8 @@ function Champs({ champs, lockedChamps = {}, toggleLock, bannedChamps = {}, togg
                             style={{
                                 position: "absolute",
                                 top: "50%",
-                                transform: "translateY(-50%)",
-                                left: 0,
-                                right: 0,
+                                transform: "translate(-50%, -50%)",
+                                left: "50%",
                                 textAlign: "center",
                             }}>
                             <Tooltip title={`'${champ.name}' is banned`}>
